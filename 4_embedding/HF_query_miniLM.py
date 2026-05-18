61d62fd03308de2c002abff31669039e0eed35b9
Shrikantlala24 <shrikantlala2023@gmail.com>
2026-05-18 14:42:11 +0530

    Update HF_query_miniLM.py

--- 4_embedding/HF_query_miniLM.py
+++ 4_embedding/HF_query_miniLM.py
@@ -1,3 +1,5 @@
+
+
 from langchain_huggingface import HuggingFaceEmbeddings
 emb = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
 
@@ -5,6 +7,7 @@ vec = emb.embed_query("test")
 
 
 try:
-    print(f"✓ Success! Embedding shape: {len(vec)}")
+    # print(f"✓ Success! Embedding shape: {len(vec)}")
+    print(str(vec))
 except Exception as e:
     print(f"✗ Error: {e}")
