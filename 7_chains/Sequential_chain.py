7aa67ea09a8edb36709efa6d802ae0fc1716b10c
Shrikantlala24 <shrikantlala2023@gmail.com>
2026-05-23 23:36:53 +0530

    Update Sequential_chain.py

--- 7_chains/Sequential_chain.py
+++ 7_chains/Sequential_chain.py
@@ -8,11 +8,14 @@ from langchain_google_genai import ChatGoogleGenerativeAI
 # Import LLM 2
 from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
 
+# Import prompt + Output parser
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.prompts import PromptTemplate
 
-
+# extras
 from dotenv import load_dotenv
+import pprint
+
 load_dotenv()
 
 
@@ -43,4 +46,66 @@ parser = StrOutputParser()
 
 seq_chain = prompt1 | llm1 | parser | prompt2 | llm2 | parser
 
-seq_chain.invoke('attention is all you need- research paper')
+response = seq_chain.invoke('attention is all you need- research paper')
+
+print(response)
+
+seq_chain.get_graph().print_ascii()
+# Here is a three-line summary of the report:
+
+# The "Attention Is All You Need" paper, presented by Vaswani et al. in 2017, introduced the Transformer architecture, which revolutionized the field of machine learning by discarding
+# recurrence and convolutions in favor of self-attention mechanisms. This led to the creation of Large Language Models (LLMs) such as BERT, GPT, and LLaMA, and has since transcended
+# Natural Language Processing (NLP) into computer vision, audio, and reinforcement learning. The Transformer's ability to parallelize computations and draw global dependencies between
+# input and output tokens enabled the training of models on web-scale data, paving the way for the modern era of Generative Artificial Intelligence.
+
+#       +-------------+      
+#       | PromptInput |      
+#       +-------------+      
+#              *             
+#              *             
+#              *             
+#     +----------------+     
+#     | PromptTemplate |     
+#     +----------------+     
+#              *             
+#              *             
+#              *             
+# +------------------------+ 
+# | ChatGoogleGenerativeAI | 
+# +------------------------+ 
+#              *             
+#              *             
+#              *             
+#     +-----------------+    
+#     | StrOutputParser |    
+#     +-----------------+    
+#              *             
+#              *             
+#              *             
+# +-----------------------+  
+# | StrOutputParserOutput |  
+# +-----------------------+  
+#              *             
+#              *             
+#              *             
+#     +----------------+     
+#     | PromptTemplate |     
+#     +----------------+     
+#              *             
+#              *             
+#              *             
+#     +-----------------+    
+#     | ChatHuggingFace |    
+#     +-----------------+    
+#              *             
+#              *             
+#              *             
+#     +-----------------+    
+#     | StrOutputParser |    
+#     +-----------------+    
+#              *             
+#              *             
+#              *             
+# +-----------------------+  
+# | StrOutputParserOutput |  
+# +-----------------------+  
